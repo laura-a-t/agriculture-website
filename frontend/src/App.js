d870384fa3b2bc39e5b837a11aa723eb06104578
laurat <laura.turcanu@gmail.com>
2020-04-09 21:54:20 +0200

    Initial code

---
+++ frontend/src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import './App.css';
+
+
+
+class App extends Component {
+
+  render() {
+    return (
+    <div className="wrapper">
+        <h3 className="display-4 lead">Bine ati venit!</h3>
+    </div>
+    );
+  }
+}
+
+export default App;
