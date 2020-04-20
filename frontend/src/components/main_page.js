211e6adf8fcfbe302ebb9433d0ea379e5d35d1e6
laurat <laura.turcanu@gmail.com>
2020-04-20 18:37:09 +0200

    Add nav tabs

--- frontend/src/components/main_page.js
+++ frontend/src/components/main_page.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Container from '@material-ui/core/Container';
 import './main_page.css';
+import NavTabs from './nav_tabs.js';
 
 
 const MainPage = () => (
             <Container maxWidth="md">
             <img src={require('../images/wheat_field.jpg')} alt="" className="banner"/>
+            <NavTabs/>
             </Container>
         );
 
---
+++ frontend/src/components/nav_tabs.js
@@ -0,0 +1,102 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <Typography
+      component="div"
+      role="tabpanel"
+      hidden={value !== index}
+      id={`nav-tabpanel-${index}`}
+      aria-labelledby={`nav-tab-${index}`}
+      {...other}
+    >
+      {value === index && <Box p={3}>{children}</Box>}
+    </Typography>
+  );
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.any.isRequired,
+  value: PropTypes.any.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `nav-tab-${index}`,
+    'aria-controls': `nav-tabpanel-${index}`,
+  };
+}
+
+function LinkTab(props) {
+  return (
+    <Tab
+      component="a"
+      onClick={(event) => {
+        event.preventDefault();
+      }}
+      {...props}
+    />
+  );
+}
+
+const generalStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.paper,
+    color: '#688054'
+  },
+}));
+
+const tabStyles = makeStyles((theme) => ({
+  root: {
+    background: '#688054',
+    indicator: '#fcfffa'
+  },
+}));
+
+export default function NavTabs() {
+  const general_styles = generalStyles();
+  const tab_styles = tabStyles();
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <div className={general_styles.root}>
+      <AppBar position="static">
+        <Tabs
+          variant="fullWidth"
+          className={tab_styles.root}
+          value={value}
+          onChange={handleChange}
+          aria-label="nav tabs example"
+        >
+          <LinkTab label="Page One" href="/drafts" {...a11yProps(0)} />
+          <LinkTab label="Page Two" href="/trash" {...a11yProps(1)} />
+          <LinkTab label="Page Three" href="/spam" {...a11yProps(2)} />
+        </Tabs>
+      </AppBar>
+      <TabPanel value={value} index={0}>
+        Page One
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+        Page Two
+      </TabPanel>
+      <TabPanel value={value} index={2}>
+        Page Three
+      </TabPanel>
+    </div>
+  );
+}
