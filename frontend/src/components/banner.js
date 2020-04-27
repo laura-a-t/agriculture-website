898aa7f5ccdae38c09d342e3db47b6a82fdd3b65
laurat <laura.turcanu@gmail.com>
2020-04-27 22:21:21 +0200

    Trying to find where to put the language buttons

--- frontend/src/components/banner.js
+++ frontend/src/components/banner.js
@@ -1,10 +1,8 @@
 import React from 'react';
-import {LanguageSelect} from './language_buttons.js'
 
 
 export const Banner = () => (
-    <React.Fragment className="container">
-    <img src={require('../images/wheat_field.jpg')} alt="" className="banner"/>
-    <LanguageSelect  className="languageSelect"/>
+    <React.Fragment>
+    <img src={require('../images/wheat_field.jpg')} alt="" className="bannerImage"/>
     </React.Fragment>
 )
--- frontend/src/components/main_page.js
+++ frontend/src/components/main_page.js
@@ -2,18 +2,19 @@ import React from 'react';
 import Container from '@material-ui/core/Container';
 import { MuiThemeProvider } from '@material-ui/core';
 import {theme} from '../css/themes.js'
-import './main_page.css';
 import NavTabs from './nav_tabs.js';
 import {Banner} from './banner.js';
+import {LanguageSelect} from './language_buttons.js'
 
 
 const MainPage = () => (
-            <Container maxWidth="md">
-                <MuiThemeProvider theme={theme}>
+            <MuiThemeProvider theme={theme}>
+                <Container maxWidth="md">
                     <Banner/>
                     <NavTabs/>
-                </MuiThemeProvider>
-            </Container>
+                </Container>
+                <LanguageSelect/>
+            </MuiThemeProvider>
         );
 
 export { MainPage };
